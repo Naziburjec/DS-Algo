2ac365635342521cf12aaea135902c792f1eb4e8
Naziburjec <naziburjec63@gmail.com>
2018-10-23 11:52:20 +0530

    Update TwoMissingNum.java
    Forgot to add 2

--- 1D-Array/TwoMissingNum.java
+++ 1D-Array/TwoMissingNum.java
@@ -5,13 +5,13 @@ public class TwoMissingNum {
       array[arr[i]]=true;
     for(int i=1;i<=n;i++)
       if(!array[i])
-        System.out.print(i+", ");
+        System.out.print(i+" ");
         
  }
 
   public static void main(String [] args) {
     int arr[] = {1, 3, 5, 6}; 
-    int n = arr.length;
+    int n = 2 + arr.length;
     findTwoMissingNums(arr,n);
   }
 }
